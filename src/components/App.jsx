569ecd5a8bf85a0a18762f4701de1fa4ed97c2c7
Kateryna Dolotova <104764377+DolotovaK@users.noreply.github.com>
2022-12-24 19:43:02 +0200

    update with hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,102 @@
-import { Component } from "react";
+import { useState } from "react";
 import { FeedbackOptions } from "./FeedbackOptions/FeedbackOptions";
 import { Section } from "./Section/Section";
 import { Statistic } from "./Statistic/Statistic";
 
-export class App extends Component{
-   
-state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
 
-  handleFeedback = evt => {
-    const currentType = evt.currentTarget.name;
-    this.setState(prevState => ({ [currentType]: prevState[currentType] + 1 }));
-    // console.log(5)
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const options = ['good', 'neutral', 'bad'];
+
+  function handleFeedback (option) {
+    switch (option) {
+      case 'good':
+        setGood(state => state + 1);
+        break;
+      case 'neutral':
+        setNeutral(state => state + 1);
+        break;
+      case 'bad':
+        setBad(state => state + 1)
+        break;
+      
+        default:
+        throw new Error(`There is no ${option}`);
+    }
   };
+
+  const totalFeedback = good + neutral + bad;
   
-  countTotalFeedback() {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  }
-
-  countPositiveFeedbackPercentage() {
-    const { good } = this.state;
-    return Math.round(good / this.countTotalFeedback() * 100);
-  }
-
-  render() {
-    return (
+  const positiveFeedbackPercentage = Math.round(good / totalFeedback * 100);
+  
+return (
       <>
       <Section title="Please leave feedback">
-        <FeedbackOptions onHandleFeedback={ this.handleFeedback} options = {['good', 'neutral', 'bad'] } />
+        <FeedbackOptions onHandleFeedback={handleFeedback} options = {options } />
         </Section>
         
       <Section title="Statistic:">  
         <Statistic
-          good={this.state.good}
-          neutral={this.state.neutral}
-          bad={this.state.bad}
-          total={this.countTotalFeedback()}
-          positivePercentage={this.countPositiveFeedbackPercentage()} />
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalFeedback}
+          positivePercentage={positiveFeedbackPercentage} />
       </Section>
-       
-        {/* <p>Statistic</p>
-        <ul>
-          <li>Good: {this.state.good}</li>
-          <li>Neutural: {this.state.neutral}</li>
-          <li>Bad: {this.state.bad}</li>
-          <li>Total: {this.countTotalFeedback()}</li>
-          <li>Positive persentage: </li>
-        </ul> */}
       </>
       
     )
-  }
+
 }
+
+
+// export class App extends Component{
+   
+// state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0
+//   }
+
+  // handleFeedback = evt => {
+  //   const currentType = evt.currentTarget.name;
+  //   this.setState(prevState => ({ [currentType]: prevState[currentType] + 1 }));
+  //   // console.log(5)
+  // };
+  
+//   countTotalFeedback() {
+//     const { good, neutral, bad } = this.state;
+//     return good + neutral + bad;
+//   }
+
+//   countPositiveFeedbackPercentage() {
+//     const { good } = this.state;
+//     return Math.round(good / this.countTotalFeedback() * 100);
+//   }
+
+//   render() {
+//     return (
+//       <>
+//       <Section title="Please leave feedback">
+//         <FeedbackOptions onHandleFeedback={ this.handleFeedback} options = {['good', 'neutral', 'bad'] } />
+//         </Section>
+        
+//       <Section title="Statistic:">  
+//         <Statistic
+//           good={this.state.good}
+//           neutral={this.state.neutral}
+//           bad={this.state.bad}
+//           total={this.countTotalFeedback()}
+//           positivePercentage={this.countPositiveFeedbackPercentage()} />
+//       </Section>
+//       </>
+      
+//     )
+//   }
+// }
 
 
 
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,11 +1,11 @@
 import { PropTypes } from "prop-types"
 import css from "./FeedbackOptions.module.css";
 
-export const FeedbackOptions = ({ options, onHandleFeedback }) => {
+export const FeedbackOptions = ({ options, onHandleFeedback }) =>{
     return (
         <section>
             {options.map((option, idx) => {
-                return <button type="button" key={idx} onClick={onHandleFeedback} name={option} className={css.button} >{option}</button>
+                return <button type="button" key={idx} onClick={()=>onHandleFeedback(option)} className={css.button}>{option}</button>
             })}
 
         </section>
